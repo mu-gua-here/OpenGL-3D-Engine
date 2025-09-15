8dcbc5e67c81dce763abd80989e6a43b5b701fef
mu_gua_here <mu_gua_the_pro_noob@gmail.com>
2025-09-15 21:13:40 +0800

    Initial Commit

---
+++ OpenGL 3D Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL 3D Engine
+//
+//  Created by Ray Hsiao Muguang on 2025/9/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
